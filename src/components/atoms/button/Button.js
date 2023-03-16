b86c4bfb148d3eb0bae4ca761d44867d94811a50
Mauricio Ocampo <mauricio.ocampo@nuvu.cc>
2023-03-16 10:05:06 -0500

    Se actualiza átomo botón

--- src/components/atoms/button/Button.js
+++ src/components/atoms/button/Button.js
@@ -3,10 +3,8 @@ import { ButtonType, ButtonSize } from "../../themes";
 
 function Button({ size, type, label, path }) {
 
-    const classNames = ButtonType[type] + " " + ButtonSize[size];
-
     return (
-        <button className={classNames}><Link href={path}>{label.toUpperCase()}</Link></button>
+        <button className={`${ButtonType?.[type]} ${ButtonSize?.[size]}`}><Link href={path}>{label.toUpperCase()}</Link></button>
     )
 }
 export default Button;
