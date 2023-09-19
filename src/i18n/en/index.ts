9ba3bbf7712c77dfd56f3380284ceda141bc4d07
cristian.sanchez <cristian.sanchez@nuvu.cc>
2023-09-19 11:55:00 -0500

    add change get involvet and aboutus

--- src/i18n/en/index.ts
+++ src/i18n/en/index.ts
@@ -1,17 +1,21 @@
-import { type I18nData } from "../type";
+import { type I18nData } from '../type'
 
-import { home } from "./home";
-import { howItWorks } from "./HowItWorks";
-import { dashboard } from "./dashboard";
-import { jobDescription } from "./jobDescription";
-import { ourPublications } from "./ourPublications";
-import { ourTechnology } from "./ourTecnology";
+import { home } from './home'
+import { AboutUs } from './AboutUs'
+import { GetInvolved_ } from './GetInvolved'
+import { howItWorks } from './HowItWorks'
+import { dashboard } from './dashboard'
+import { jobDescription } from './jobDescription'
+import { ourPublications } from './ourPublications'
+import { ourTechnology } from './ourTecnology'
 export const data: I18nData = {
-  locale: "en",
+  locale: 'en',
   home,
+  AboutUs,
+  GetInvolved_,
   howItWorks,
   dashboard,
   jobDescription,
   ourPublications,
-  ourTechnology
-};
+  ourTechnology,
+}
