f4cc8041e93129a2e32cf4bd5a028bdd2478cc4c
Valentina Mancipe V <valentina.mancipe@nuvu.cc>
2023-09-19 09:34:27 -0500

    Delete lenguaje japones y portugues

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,25 +3,18 @@ import { type I18nData } from './type'
 
 import { data as enData } from './en'
 import { data as esData } from './es'
-import { data as ptData } from './pt'
-import { data as jpData } from './jp'
 
-export type Locales = 'en' | 'es' //| "pt" | "jp"//
+export type Locales = 'en' | 'es'
 export const defaultLocale: Locales = 'en'
 
 export const localeData = {
   en: enData,
   es: esData,
-  // pt: ptData,
-  // jp: jpData,
 }
 
 export const localeOptions = [
   { value: 'en', label: 'ENG' },
   { value: 'es', label: 'ESP' },
-//TODO: AJUSTAR
-  { value: 'pt', label: 'JPN' },
-  { value: 'jp', label: 'POR' },
 ]
 
 export function useI18n(): I18nData {
