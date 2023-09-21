29743621b7736bc8a9485503dd454e561f8d61de
cristian.sanchez <cristian.sanchez@nuvu.cc>
2023-09-21 15:32:49 -0500

    add info to the blogDetails  sections

--- src/i18n/en/BlogDetail.ts
+++ src/i18n/en/BlogDetail.ts
@@ -24,31 +24,34 @@ export const blogDetails: BlogDetails = {
       id: 2,
       title: 'Trust is AI’s Most Critical Contribution to Health Care',
       category: '',
-      date: '',
-      autor: '',
-      image: '',
+      date: 'September 21, 2023',
+      autor: 'Cristian Sanchez',
+      image: '/images/card2.jpg',
       description:
-        'AI can reveal remarkable medical insights, but only if patients and doctors have faith in it. Thus, trust has become AI’s singular goal, says Stanfords James Zou.',
+        'AI can reveal remarkable medical insights, but only if patients and doctors have faith in it. Thus, trust has become AI’s singular goal, says Stanfords James Zou.' +
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
     },
     {
       id: 3,
       title: 'Coronavirus disease (COVID-19): Post COVID-19 condition',
       category: '',
-      date: '',
-      autor: '',
-      image: '',
+      date: 'September 21, 2023',
+      autor: 'Cristian Sanchez',
+      image: '/images/card3.jpg',
       description:
-        'Most people who develop COVID-19 fully recover, but current evidence suggests approximately 10%-20% of people experience a variety of mid- and long-term effects.',
+        'AI can reveal remarkable medical insights, but only if patients and doctors have faith in it. Thus, trust has become AI’s singular goal, says Stanfords James Zou.' +
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
     },
     {
       id: 4,
       title: 'Trust is AI’s Most Critical Contribution to Health Care',
       category: '',
-      date: '',
-      autor: '',
-      image: '',
+      date: 'September 21, 2023',
+      autor: 'Cristian Sanchez',
+      image: '/images/card4.jpg',
       description:
-        'The COVID-19 pandemic continues to present considerable public health challenges in the United States and around the globe.',
+        'AI can reveal remarkable medical insights, but only if patients and doctors have faith in it. Thus, trust has become AI’s singular goal, says Stanfords James Zou.' +
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
     },
   ],
 }
--- src/i18n/es/BlogDetails.ts
+++ src/i18n/es/BlogDetails.ts
@@ -10,48 +10,49 @@ export const blogDetails: BlogDetails = {
       autor: 'Katherine Miller',
       image: '/images/card1.jpg',
       description:
-        'En el gran esquema de las operaciones de atención médica, los modelos predictivos desempeñan un papel no muy diferente al de las pruebas de sangre, radiografías o resonancias magnéticas: influyen en decisiones sobre si una intervención es apropiada. ' +
-        '“En términos generales, los modelos hacen matemáticas y generan estimaciones de probabilidad que te ayudan a decidir si actuar”, dice Nigam Shah, Científico de Datos Principal de Stanford Health Care y miembro de la facultad de Stanford HAI. ' +
-        'Pero esas estimaciones de probabilidad solo son útiles para los proveedores de atención médica si generan decisiones más beneficiosas que perjudiciales. ' +
-        '“Como comunidad, creo que estamos centrados en el rendimiento del modelo y no estamos haciendo la pregunta, ¿Es útil el modelo?”, dice Shah. “Necesitamos pensar más allá del modelo”. ' +
-        'El equipo de Shah es uno de los pocos grupos de investigación en atención médica que evalúa si los hospitales tienen la capacidad de intervenir basándose en un modelo y si, en general, las intervenciones serán beneficiosas para pacientes e instituciones de atención médica. ' +
-        '“Existe una creciente preocupación de que los investigadores de IA estén construyendo modelos a diestra y siniestra y que nada se implemente”, dice Shah. Una de las razones de esto es el fracaso de los modeladores para realizar un análisis de utilidad que muestre cómo la intervención desencadenada por un modelo se ajustará a las operaciones hospitalarias de manera rentable y al mismo tiempo hará menos daño que bien. ' +
-        '“Si los desarrolladores de modelos dedicaran tiempo a realizar este análisis adicional, los hospitales prestarían atención”, dice. ' +
-        'Las herramientas para realizar un análisis de utilidad ya existen en los campos de la investigación de operaciones, política de atención médica y econometría, pero los desarrolladores de modelos en atención médica han sido lentos en usarlas, según Shah. Su propio equipo ha intentado cambiar esa mentalidad publicando varios artículos instando a más personas a evaluar la utilidad de sus modelos. Estos incluyen un artículo de JAMA que establece la necesidad de que los modeladores piensen en la utilidad y un artículo de investigación que presenta un marco para analizar la utilidad de los modelos predictivos en atención médica y demuestra cómo funcionaría utilizando un ejemplo del mundo real. ' +
-        '“Al igual que cualquier cosa nueva que un hospital pueda agregar a sus operaciones, implementar un nuevo modelo debe valer la pena”, dice Shah. “Existen marcos sólidos para determinar el valor de un modelo. Es hora de que los modeladores los utilicen”.',
+        'En el gran esquema de las operaciones de atención médica, los modelos predictivos desempeñan un papel no muy diferente al de los análisis de sangre, las radiografías o las resonancias magnéticas: influyen en las decisiones sobre si una intervención es apropiada. ' +
+        '“Hablando en términos generales, los modelos hacen matemáticas y generan estimaciones de probabilidad que te ayudan a decidir si actuar”, dice Nigam Shah, Chief Data Scientist de Stanford Health Care y miembro de la facultad de Stanford HAI. ' +
+        'Pero esas estimaciones de probabilidad solo son útiles para los proveedores de atención médica si desencadenan decisiones que son más beneficiosas que perjudiciales. ' +
+        '“Como comunidad, creo que estamos obsesionados con el rendimiento del modelo y no estamos haciendo la pregunta, ¿Es útil el modelo?”, dice Shah. “Necesitamos pensar más allá del modelo”. ' +
+        'El equipo de Shah es uno de los pocos grupos de investigación en atención médica que evalúa si los hospitales tienen la capacidad de intervenir en función de un modelo y si, en general, las intervenciones serán beneficiosas para los pacientes y las instituciones de atención médica. ' +
+        '“Existe una creciente preocupación de que los investigadores en IA estén construyendo modelos a diestra y siniestra y que nada se esté implementando”, dice Shah. Una de las razones de esto es el fracaso de los modeladores para realizar un análisis de utilidad que muestre cómo encajará la intervención desencadenada por un modelo en las operaciones del hospital de manera rentable y al mismo tiempo hará menos daño que bien. ' +
+        '“Si los desarrolladores de modelos se tomaran el tiempo para realizar este análisis adicional, los hospitales prestarán atención”, dice. ' +
+        'Las herramientas para realizar un análisis de utilidad ya existen en los campos de investigación de operaciones, políticas de atención médica y econometría, sin embargo, los desarrolladores de modelos en el ámbito de la atención médica han sido lentos en utilizarlas, dice Shah. Su propio equipo ha intentado cambiar esa mentalidad publicando varios documentos instando a más personas a evaluar la utilidad de sus modelos. Estos incluyen un artículo de JAMA que establece la necesidad de que los modeladores piensen en la utilidad y un artículo de investigación que describe un marco para analizar la utilidad de los modelos predictivos en la atención médica y demuestra cómo funcionaría utilizando un ejemplo del mundo real. ' +
+        '“Como cualquier cosa nueva que un hospital pueda agregar a sus operaciones, implementar un nuevo modelo debe valer la pena”, dice Shah. “Existen marcos sólidos para determinar el valor de un modelo. Es hora de que los modeladores los utilicen”.',
     },
     {
       id: 2,
       title:
-        'La confianza es la contribución más crítica de la IA en la atención médica',
+        'La confianza es la contribución más crítica de la IA a la atención médica',
       category: '',
-      date: '',
-      autor: '',
-      image: '',
+      date: '21 de septiembre de 2023',
+      autor: 'Cristian Sanchez',
+      image: '/images/card2.jpg',
       description:
-        'La IA puede revelar conocimientos médicos sorprendentes, pero solo si los pacientes y médicos tienen fe en ella. Por lo tanto, la confianza se ha convertido en el objetivo único de la IA, dice James Zou de Stanford.',
+        'La IA puede revelar conocimientos médicos sorprendentes, pero solo si los pacientes y los médicos tienen confianza en ella. Por lo tanto, la confianza se ha convertido en el objetivo único de la IA, según James Zou de Stanford.' +
+        "Lorem Ipsum es simplemente un texto ficticio de la industria de la impresión y la composición tipográfica. Lorem Ipsum ha sido el texto ficticio estándar de la industria desde la década de 1500, cuando un impresor desconocido tomó un conjunto de tipos y lo mezcló para crear un libro de muestras de tipos. Ha sobrevivido no solo cinco siglos, sino también la transición a la composición tipográfica electrónica, manteniéndose esencialmente sin cambios. Se popularizó en la década de 1960 con el lanzamiento de hojas de Letraset que contenían pasajes de Lorem Ipsum, y más recientemente con software de autoedición como Aldus PageMaker, que incluía versiones de Lorem Ipsum. Es un hecho establecido que un lector se distraerá con el contenido legible de una página cuando observe su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de letras, en lugar de usar 'Contenido aquí, contenido aquí', lo que hace que parezca un texto en inglés legible. Muchos paquetes de autoedición y editores de páginas web ahora usan Lorem Ipsum como texto de modelo predeterminado, y una búsqueda de 'Lorem Ipsum' revelará muchos sitios web que aún están en su infancia. Varias versiones han evolucionado a lo largo de los años, a veces por accidente, a veces a propósito (se inyecta humor y cosas similares). Contrariamente a la creencia popular, Lorem Ipsum no es simplemente un texto aleatorio. Tiene raíces en una pieza de literatura latina clásica escrita en el año 45 a.C. por Cicerón, llamada de Finibus Bonorum et Malorum (Los Extremos del Bien y el Mal). Este libro es un tratado sobre la teoría de la ética, muy popular durante el Renacimiento. La primera línea de Lorem Ipsum, 'Lorem ipsum dolor sit amet...', proviene de una línea en la sección 1.10.32.",
     },
     {
       id: 3,
-      title:
-        'Enfermedad por coronavirus (COVID-19): Condición posterior a la COVID-19',
+      title: 'Enfermedad por coronavirus (COVID-19): Condición post COVID-19',
       category: '',
-      date: '',
-      autor: '',
-      image: '',
+      date: '21 de septiembre de 2023',
+      autor: 'Cristian Sanchez',
+      image: '/images/card3.jpg',
       description:
-        'La mayoría de las personas que desarrollan COVID-19 se recuperan completamente, pero la evidencia actual sugiere que aproximadamente el 10%-20% de las personas experimenta una variedad de efectos a medio y largo plazo.',
+        'La IA puede revelar conocimientos médicos sorprendentes, pero solo si los pacientes y los médicos tienen confianza en ella. Por lo tanto, la confianza se ha convertido en el objetivo único de la IA, según James Zou de Stanford.' +
+        "Lorem Ipsum es simplemente un texto ficticio de la industria de la impresión y la composición tipográfica. Lorem Ipsum ha sido el texto ficticio estándar de la industria desde la década de 1500, cuando un impresor desconocido tomó un conjunto de tipos y lo mezcló para crear un libro de muestras de tipos. Ha sobrevivido no solo cinco siglos, sino también la transición a la composición tipográfica electrónica, manteniéndose esencialmente sin cambios. Se popularizó en la década de 1960 con el lanzamiento de hojas de Letraset que contenían pasajes de Lorem Ipsum, y más recientemente con software de autoedición como Aldus PageMaker, que incluía versiones de Lorem Ipsum. Es un hecho establecido que un lector se distraerá con el contenido legible de una página cuando observe su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de letras, en lugar de usar 'Contenido aquí, contenido aquí', lo que hace que parezca un texto en inglés legible. Muchos paquetes de autoedición y editores de páginas web ahora usan Lorem Ipsum como texto de modelo predeterminado, y una búsqueda de 'Lorem Ipsum' revelará muchos sitios web que aún están en su infancia. Varias versiones han evolucionado a lo largo de los años, a veces por accidente, a veces a propósito (se inyecta humor y cosas similares). Contrariamente a la creencia popular, Lorem Ipsum no es simplemente un texto aleatorio. Tiene raíces en una pieza de literatura latina clásica escrita en el año 45 a.C. por Cicerón, llamada de Finibus Bonorum et Malorum (Los Extremos del Bien y el Mal). Este libro es un tratado sobre la teoría de la ética, muy popular durante el Renacimiento. La primera línea de Lorem Ipsum, 'Lorem ipsum dolor sit amet...', proviene de una línea en la sección 1.10.32.",
     },
     {
       id: 4,
-      title:
-        'La confianza es la contribución más crítica de la IA en la atención médica',
+      title: 'La confianza es la contribución más crítica de la IA a la atención médica',
       category: '',
-      date: '',
-      autor: '',
-      image: '',
+      date: '21 de septiembre de 2023',
+      autor: 'Cristian Sanchez',
+      image: '/images/card4.jpg',
       description:
-        'La pandemia de COVID-19 continúa presentando importantes desafíos de salud pública en los Estados Unidos y en todo el mundo.',
+        'La IA puede revelar conocimientos médicos sorprendentes, pero solo si los pacientes y los médicos tienen confianza en ella. Por lo tanto, la confianza se ha convertido en el objetivo único de la IA, según James Zou de Stanford.' +
+        "Lorem Ipsum es simplemente un texto ficticio de la industria de la impresión y la composición tipográfica. Lorem Ipsum ha sido el texto ficticio estándar de la industria desde la década de 1500, cuando un impresor desconocido tomó un conjunto de tipos y lo mezcló para crear un libro de muestras de tipos. Ha sobrevivido no solo cinco siglos, sino también la transición a la composición tipográfica electrónica, manteniéndose esencialmente sin cambios. Se popularizó en la década de 1960 con el lanzamiento de hojas de Letraset que contenían pasajes de Lorem Ipsum, y más recientemente con software de autoedición como Aldus PageMaker, que incluía versiones de Lorem Ipsum. Es un hecho establecido que un lector se distraerá con el contenido legible de una página cuando observe su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de letras, en lugar de usar 'Contenido aquí, contenido aquí', lo que hace que parezca un texto en inglés legible. Muchos paquetes de autoedición y editores de páginas web ahora usan Lorem Ipsum como texto de modelo predeterminado, y una búsqueda de 'Lorem Ipsum' revelará muchos sitios web que aún están en su infancia. Varias versiones han evolucionado a lo largo de los años, a veces por accidente, a veces a propósito (se inyecta humor y cosas similares). Contrariamente a la creencia popular, Lorem Ipsum no es simplemente un texto aleatorio. Tiene raíces en una pieza de literatura latina clásica escrita en el año 45 a.C. por Cicerón, llamada de Finibus Bonorum et Malorum (Los Extremos del Bien y el Mal). Este libro es un tratado sobre la teoría de la ética, muy popular durante el Renacimiento. La primera línea de Lorem Ipsum, 'Lorem ipsum dolor sit amet...', proviene de una línea en la sección 1.10.32.",
     },
   ],
 }
