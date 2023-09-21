2eaba35b393d194dff793057474ff39f6a7d7f03
cristian.sanchez <cristian.sanchez@nuvu.cc>
2023-09-21 12:04:40 -0500

    add language traslate and type for navbar

---
+++ src/i18n/types/NavbarType.ts
@@ -0,0 +1,32 @@
+export interface NavbarType {
+    ourTecnology: OurTecnology
+    getInvolved: GetInvolved
+    aboutUs: AboutUs
+    faq: string
+    joinUs: string
+    donate: string
+}
+
+export interface OurTecnology {
+    section: string
+    howItWorks: string
+    shareYourCough: string
+    OurData: string
+    OurResearch: string
+}
+
+export interface GetInvolved {
+    section: string
+    donate: string
+    joinOurTeam: string
+    tellYourStory: string
+}
+
+export interface AboutUs {
+    section: string
+    ourPeople: string
+    ourSopporters: string
+    blog: string
+    publications: string
+}
+
