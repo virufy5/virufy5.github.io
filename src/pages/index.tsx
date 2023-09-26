6a57f4f22a8c3d6cc0504aa17b7d0c7d532c9d0e
David Villarreal <David.Villareal@nuvu.cc>
2023-09-26 09:14:53 -0500

    Ajuste home en raiz

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,10 @@
-// import LocaleSelector from "~/components/LocaleSelector";
-import LocaleSelector from "~/components/atoms/Select/LocaleSelector";
-import { useI18n } from "~/i18n";
+import Home from "./home"
 
-export default function Home() {
-  // const {
-  //   home: { title, subtitle },
-  // } = useI18n();
+export default function index() {
 
   return (
     <>
-      <LocaleSelector />
-      <iframe
-        src={`https://virufy-dashboard.vercel.app/`}
-        className="w-full min-h-screen border-none"
-      />
-      {/* <h1>{title} </h1>
-      <p>{subtitle}</p> */}
+    <Home/>
     </>
   );
 }
