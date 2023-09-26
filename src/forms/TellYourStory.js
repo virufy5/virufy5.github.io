1fa889a93778ac57050b69411ef32e5e537631f9
David Villarreal <David.Villareal@nuvu.cc>
2023-09-26 11:01:45 -0500

    Ajuste Form TellYourStory

--- src/forms/TellYourStory.js
+++ src/forms/TellYourStory.js
@@ -1,6 +1,6 @@
 export const formIDs = {
-  url: 'https://docs.google.com/forms/d/1cSlgCcDSc4EPcF-gYD6PR2dNUbvmLFQNOBh80UHeW0g/formResponse',
-  name: 'entry.1149158785',
-  email: 'entry.1955664903',
-  message: 'entry.1134294355',
+  url: 'https://docs.google.com/forms/d/1-pawgyz0ey8cnRUQRfrLNo3pxd_zUnZjuBqMnS8kQWo/formResponse',
+  name: 'entry.1771868251',
+  email: 'entry.1675966084',
+  message: 'entry.2083626667',
 }
