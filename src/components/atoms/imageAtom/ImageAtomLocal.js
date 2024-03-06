da591964cbee647010a6287ee00a461503b60d9e
gevuong <gevuong5627@gmail.com>
2024-03-05 19:19:01 -0800

    format code and fix typo in NavBar and ImageAtomLocal component

--- src/components/atoms/imageAtom/ImageAtomLocal.js
+++ src/components/atoms/imageAtom/ImageAtomLocal.js
@@ -1,11 +1,22 @@
-import { ImageSize, ImageBorder } from "../../themes";
-import Image from "next/image";
+import { ImageSize, ImageBorder } from '../../themes'
+import Image from 'next/image'
 
-export default function ImageAtomLocal({ imagesize, alt, border, src, ImageStyleProps }) {
-
+export default function ImageAtomLocal({
+  imagesize,
+  alt,
+  border,
+  src,
+  ImageStyleProps,
+}) {
   return (
     <>
-      <Image className={`${ImageBorder?.[border]} ${ImageSize?.[imagesize]} ${ImageStyleProps}`} src={src} width="0" height="0" alt={alt} />
+      <Image
+        className={`${ImageBorder?.[border]} ${ImageSize?.[imagesize]} ${ImageStyleProps}`}
+        src={src}
+        width="0"
+        height="0"
+        alt={alt}
+      />
     </>
   )
 }
